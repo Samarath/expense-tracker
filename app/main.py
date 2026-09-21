6cb760752baba63b0e7d5170c042260825d8b8e5
samarath <samrathkumarmuz@gmail.com>
2026-09-21 18:19:38 +0530

    updated cors

--- app/main.py
+++ app/main.py
@@ -16,7 +16,11 @@ app = FastAPI(title="Spend Tracker API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://expense-tracker-rho-gold-36.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
